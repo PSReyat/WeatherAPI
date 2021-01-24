576e4432d517f1c8d415d2767038260339d0f8f9
Parminder <preyat94@gmail.com>
2021-01-24 19:05:18 +0000

    minor code fixes to Service class

--- src/main/java/com/weatherapi/service/WeatherServiceImpl.java
+++ src/main/java/com/weatherapi/service/WeatherServiceImpl.java
@@ -108,12 +108,11 @@ public class WeatherServiceImpl implements WeatherService{
 			
 			List<FiveDayHourlyWeather> weatherPerThreeHoursPerDay = new ArrayList<>();
 			this.weatherForFiveDays = new LinkedHashMap<>();
+			JSONObject obj = new JSONObject(this.json);
+			FiveDayHourlyWeather hourlyWeather;
 			DateTime dt = new DateTime(new Date());
 			DateTime.Property dtp = dt.dayOfWeek();
 			String day = dtp.getAsText();
-			
-			JSONObject obj = new JSONObject(this.json);
-			FiveDayHourlyWeather hourlyWeather;
 			
 			int count = 0;
 			
