dc81d76c6efcc9d4c060f3c49fefadc86a90cd77
Parminder <preyat94@gmail.com>
2021-02-10 14:42:55 +0000

    heroku update

--- src/main/java/com/weatherapi/WeatherApiApplication.java
+++ src/main/java/com/weatherapi/WeatherApiApplication.java
@@ -2,9 +2,10 @@ package com.weatherapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WeatherApiApplication {
+public class WeatherApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApiApplication.class, args);
